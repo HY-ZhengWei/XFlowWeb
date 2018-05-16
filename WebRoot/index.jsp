fc9b65b2042b7e084b51299d503cef02ce8c349f
HY-ZhengWei <hy.zhengwei@qq.com>
2018-05-16 16:12:42 +0800

    1.0.0

---
+++ WebRoot/index.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ page import="org.hy.common.Help" %>
+<%@ page import="org.hy.common.StringHelp" %>
+<%@ page import="org.hy.common.xml.XJava" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+   
+    <title>大数据云计算系统</title>
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="expires" content="0">    
+	<meta http-equiv="keywords" content="大数据云计算系统">
+	<meta http-equiv="description" content="大数据云计算系统">
+	
+	<script type="text/javascript"> 
+	
+    function gotoMenu()
+    {
+    	window.location.href = "analyses";
+    }
+	</script>  
+	
+  </head>
+  
+  <body>
+    <script type="text/javascript">gotoMenu();</script>
+  </body>
+</html>
---
+++ src/org/hy/xflow/web/config/InitConfig.java
@@ -0,0 +1,92 @@
+package org.hy.xflow.web.config;
+
+import java.util.List;
+
+import org.hy.common.Help;
+import org.hy.common.app.Param;
+import org.hy.common.thread.ThreadPool;
+import org.hy.common.xml.XJava;
+import org.hy.common.xml.plugins.AppInitConfig;
+
+
+
+
+
+/**
+ * Web初始化信息
+ * 
+ * @author      ZhengWei(HY)
+ * @createDate  2014-09-12
+ * @version     v1.0  
+ */
+public final class InitConfig extends AppInitConfig
+{
+    
+    private static boolean $Init = false;
+    
+    private String xmlRoot;
+    
+    
+    
+    public InitConfig()
+    {
+        XJava.putObject("IsOpenServerSocket" ,Boolean.TRUE);
+        this.xmlRoot = Help.getWebINFPath();
+        init();
+    }
+    
+    
+    
+    public InitConfig(String i_XmlRoot)
+    {
+        this.xmlRoot = i_XmlRoot;
+        init();
+    }
+    
+    
+    
+    @SuppressWarnings("unchecked")
+    private synchronized void init()
+    {
+    	
+        if ( !$Init )
+        {
+            $Init = true;
+            
+            try
+            {
+                this.initW("sys.Config.xml" ,this.xmlRoot);
+                this.initW("startup.Config.xml" ,this.xmlRoot);
+                this.initW((List<Param>)XJava.getObject("StartupConfig") ,this.xmlRoot);
+                this.initW(((Param)XJava.getObject("RootPackageName")).getValue());
+                init_TPool();
+                
+                this.initW("job.Config.xml" ,this.xmlRoot);
+            }
+            catch (Exception exce)
+            {
+                System.out.println(exce.getMessage());
+                exce.printStackTrace();
+            }
+        }
+    }
+    
+    
+    
+    private void init_TPool()
+    {
+        ThreadPool.setMaxThread(    this.getIntConfig("TPool_MaxThread"));
+        ThreadPool.setMinThread(    this.getIntConfig("TPool_MinThread"));
+        ThreadPool.setMinIdleThread(this.getIntConfig("TPool_MinIdleThread"));
+        ThreadPool.setIntervalTime( this.getIntConfig("TPool_IntervalTime"));
+        ThreadPool.setIdleTimeKill( this.getIntConfig("TPool_IdleTimeKill"));
+    }
+    
+    
+    
+    private int getIntConfig(String i_XJavaID)
+    {
+        return Integer.parseInt(XJava.getParam(i_XJavaID).getValue());
+    }
+    
+}
